c9e57ca1cbb6b202f73fa9f7701b55ee93161100
Niels Koster <n.koster@portavita.eu>
2017-10-15 14:42:10 +0200

    Adding arc, and more, WIP

--- js/app.js
+++ js/app.js
@@ -6,11 +6,53 @@ function setCursorByID(id, cursorStyle) {
 
 setCursorByID('view', 'crosshair');
 
+function calcCirclePath(x1, y1, x2, y2, x3, y3){
+    var a = [x1, y1];
+    var b = [x3, y3];
+    var c = [x2, y2];
+
+    var A = dist(b, c);
+    var B = dist(c, a);
+    var C = dist(a, b);
+
+    var angle = Math.acos((A*A + B*B - C*C)/(2*A*B));
+
+    //calc radius of circle
+    var K = .5*A*B*Math.sin(angle);
+    var r = A*B*C/4/K;
+    r = Math.round(r*1000)/1000;
+
+    //large arc flag
+    var laf = +(Math.PI/2 > angle);
+
+    //sweep flag
+    var saf = +((b[0] - a[0])*(c[1] - a[1]) - (b[1] - a[1])*(c[0] - a[0]) < 0);
+
+    return ['M', a, 'A', r, r, 0, laf, saf, b].join(' ')
+}
+
+function dist(a, b){
+    return Math.sqrt(
+        Math.pow(a[0] - b[0], 2) +
+        Math.pow(a[1] - b[1], 2))
+}
+
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 var svgNS = svg.namespaceURI;
 drawing = [];
 count = 0;
-var selected = false, del = false, debug = true, escapePress = false, deletePress = false;
+var
+    selected = false,
+    del = false,
+    debug = true,
+    escapePress = false,
+    deletePress = false;
+
+var
+    drawLine = 0,
+    drawArc = 1,
+    draw = drawLine;
+var xx, yy;
 
 function mouseOver(evt) {
     var element = evt.target;
@@ -62,24 +104,45 @@ function click(evt) {
     document.getElementById('coords').innerHTML = 'objects=' + count + ', x=' + x + ', y=' + y;
     if (selected) {
         selected = false;
-        drawing[count].setAttributeNS(null, 'x2', x);
-        drawing[count].setAttributeNS(null, 'y2', y);
-        count++
+        if (draw === drawLine) {
+            drawing[count].setAttributeNS(null, 'x2', x);
+            drawing[count].setAttributeNS(null, 'y2', y);
+            count++
+        }
+        if (draw === drawArc) {
+            drawing[count].setAttributeNS(null, 'd', 'M' + xx + ',' + yy + ' C5,45 45,45 ' + x + ',' + y);
+            count++
+        }
     } else {
         selected = true;
         deletePress = false;
         setCursorByID('view', 'crosshair');
-        drawing[count] = document.createElementNS(svgNS, 'line');
-        drawing[count].setAttributeNS(null, "stroke", color);
-        drawing[count].setAttributeNS(null, "stroke-width", 3);
-        drawing[count].setAttributeNS(null, 'x1', x);
-        drawing[count].setAttributeNS(null, 'y1', y);
-        drawing[count].setAttributeNS(null, 'x2', x);
-        drawing[count].setAttributeNS(null, 'y2', y);
-        drawing[count].setAttributeNS(null, 'onmouseover', 'mouseOver(evt)');
-        drawing[count].setAttributeNS(null, 'onmouseout', 'mouseOut(evt)');
-        drawing[count].setAttributeNS(null, 'onmousedown', 'mouseDown(evt)');
-        document.getElementById('view').appendChild(drawing[count]);
+        if (draw === drawLine) {
+            drawing[count] = document.createElementNS(svgNS, 'line');
+            drawing[count].setAttributeNS(null, 'stroke', color);
+            drawing[count].setAttributeNS(null, 'stroke-width', 3);
+            drawing[count].setAttributeNS(null, 'x1', x);
+            drawing[count].setAttributeNS(null, 'y1', y);
+            drawing[count].setAttributeNS(null, 'x2', x);
+            drawing[count].setAttributeNS(null, 'y2', y);
+            drawing[count].setAttributeNS(null, 'onmouseover', 'mouseOver(evt)');
+            drawing[count].setAttributeNS(null, 'onmouseout', 'mouseOut(evt)');
+            drawing[count].setAttributeNS(null, 'onmousedown', 'mouseDown(evt)');
+            document.getElementById('view').appendChild(drawing[count])
+        }
+        if (draw === drawArc) {
+            xx = x; yy = y;
+            drawing[count] = document.createElementNS(svgNS, "path");
+            drawing[count].setAttributeNS(null, 'stroke', color);
+            drawing[count].setAttributeNS(null, 'stroke-width', 3);
+            //drawing[count].setAttributeNS(null, 'd', 'M' + xx + ',' + yy + ' C5,45 45,45 ' + x + ',' + y);
+            drawing[count].setAttributeNS(null, 'd', calcCirclePath(xx,yy, xx,yy, x,y));
+            drawing[count].setAttributeNS(null, 'fill', 'none');
+            drawing[count].setAttributeNS(null, 'onmouseover', 'mouseOver(evt)');
+            drawing[count].setAttributeNS(null, 'onmouseout', 'mouseOut(evt)');
+            drawing[count].setAttributeNS(null, 'onmousedown', 'mouseDown(evt)');
+            document.getElementById('view').appendChild(drawing[count]);
+        }
     }
 }
 
@@ -89,9 +152,16 @@ function getCoords(evt) {
     var y = (evt.clientY - myProps.top).toFixed();
     document.getElementById('coords').innerHTML = 'objects=' + count + ', x=' + x + ', y=' + y;
     if (selected) {
-        if (drawing[count]) {
-            drawing[count].setAttributeNS(null, 'x2', x);
-            drawing[count].setAttributeNS(null, 'y2', y)
+        if (draw === drawLine) {
+            if (drawing[count]) {
+                drawing[count].setAttributeNS(null, 'x2', x);
+                drawing[count].setAttributeNS(null, 'y2', y)
+            }
+        }
+        if (draw === drawArc) {
+            if (drawing[count]) {
+                drawing[count].setAttributeNS(null, 'd', calcCirclePath(xx,yy, x,y, 100,100));
+            }
         }
     }
 }
